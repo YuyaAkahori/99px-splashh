226a74fbe8444e1839eed641e8df62db77cbee66
YuyaAkahori <75971297+YuyaAkahori@users.noreply.github.com>
2020-12-20 00:19:24 +0900

    v0.05
    html大幅変更

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+var current;
+$.scrollify({
+    section:".ctc",
+    setHeights:false,
+    before:function(i,box){
+        current = i;
+    },
+});
+$(window).on('resize',function(){
+    if(current){
+        var currentScrl = $('.ctc').eq(current).offset().top;
+        $(window).scrollTop(currentScrl);
+    }
+});
+
 var winH = $(window).height();
 $('.box').outerHeight(winH);
  
@@ -12,30 +27,23 @@ $(window).on('resize',function(){
 });
  
 function setBgImg(object){
-    //画像サイズ取得
     var imgW = object.width();
     var imgH = object.height();
  
-    //ウィンドウサイズ取得
     var winW = $(window).width();
     var winH = $(window).height();  
- 
-    //幅・高さの拡大率取得
+
     var scaleW = winW / imgW;
     var scaleH = winH / imgH;
- 
-    //幅・高さの拡大率の大きいものを取得
+
     var fixScale = Math.max(scaleW, scaleH);
  
-    //画像の幅高さを設定
     var setW = imgW * fixScale;
     var setH = imgH * fixScale;
  
-    //画像の位置を設定
     var moveX = Math.floor((winW - setW) / 2);
     var moveY = Math.floor((winH - setH) / 2);
  
-    //設定した数値でスタイルを適用
     object.css({
         'width': setW,
         'height': setH,
---
+++ js/test.js
@@ -0,0 +1,1 @@
+$.scrollify({section:".box"});
